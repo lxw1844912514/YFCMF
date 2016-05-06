cb28d419bf4991ae51d539b33500b865e3104703
rainfer <81818832@qq.com>
2016-05-06 21:07:59 +0800

    init

--- app/Home/Model/NewsModel.class.php
+++ app/Home/Model/NewsModel.class.php
@@ -4,17 +4,13 @@ use Think\Model\RelationModel;
 
 class NewsModel extends RelationModel{
 	protected $_link=array(
-		'column' => array(
+		'menu' => array(
 			'mapping_type'  => self::BELONGS_TO,
-			'class_name'    => 'column',
+			'class_name'    => 'menu',
 			'foreign_key'   => 'news_columnid',
-			'as_fields'  => 'column_name',
+			'as_fields'  => 'menu_name',
 		),
 	);
 
 }
-
-
-
-
 ?>
