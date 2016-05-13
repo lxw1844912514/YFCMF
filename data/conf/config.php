9a53c17a227bd13c0f158434c3ca9d67f532d6a7
Rainfer <81818832@qq.com>
2016-05-13 10:07:33 +0800

    修复路径

--- data/conf/config.php
+++ data/conf/config.php
@@ -9,17 +9,16 @@
     'con/:n_id' => 'Home/Index/news_content',
     'list/:c_id' => 'Home/Index/news_list',
   ),
-  'TMPL_PARSE_STRING' => 
-  array (
-    '__UPLOAD__' => '/ace/data/upload/',
-    '__PUBLIC__' => '/ace/public',
-    '__RES__' => '/ace/res/',
-    '__DATA__' => '/ace/data/',
-    '__AVATAR__' => '/ace/data/image/avatar/',
-    '__STATIC_ROOT__' => '/ace',
-    '__JS_SUFFIX__' => '.src.js',
-    '__CSS_SUFFIX__' => '.src.css',
-  ),
+  'TMPL_PARSE_STRING' => array(
+	'__UPLOAD__' => __ROOT__ . '/data/upload/',
+	'__PUBLIC__'=>__ROOT__ . '/public'
+	'__RES__' => __ROOT__ . '/res/',
+	'__DATA__' => __ROOT__ . '/data/',
+	'__AVATAR__' => __ROOT__ . '/data/image/avatar/',
+	'__STATIC_ROOT__' => __ROOT__,
+	'__JS_SUFFIX__' => (APP_DEBUG ? '.src.js' : '.js'),
+	'__CSS_SUFFIX__' => (APP_DEBUG ? '.src.css' : '.css')
+    ),
   'UPLOAD_TEMP_DIR' => './data/runtime/Data/',
   'UPLOAD_DIR' => './data/upload/',
   'YFCMF_VERSION' => 'V1.0.0',
