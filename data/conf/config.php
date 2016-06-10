962653511f8d8a39bb474263ec7fa2b9ed7b46a7
rainfer <81818832@qq.com>
2016-06-10 14:33:48 +0800

    更新版本号

--- data/conf/config.php
+++ data/conf/config.php
@@ -3,31 +3,31 @@
   'URL_CASE_INSENSITIVE' => true,
   'DB_LIKE_FIELDS' => 'news_title|news_content|news_flag|news_open',
   'URL_MODEL' => 0,
-  'URL_HTML_SUFFIX' => 'html',
+  'URL_HTML_SUFFIX' => '',
   'URL_ROUTER_ON' => true,
   'URL_MAP_RULES' => 
   array (
-    'index' => 'home/index/index',
-    'contacts' => 'home/list/index?id=10',
     'about' => 'home/list/index?id=1',
+    'contacts' => 'home/list/index?id=10',
+    'index' => 'home/index/index',
   ),
   'URL_ROUTE_RULES' => 
   array (
-    'news/:id\\d' => 'home/news/index',
     'list/:id\\d' => 'home/list/index',
+    'news/:id\\d' => 'home/news/index',
   ),
-  'YFCMF_VERSION' => 'V1.1.1',
+  'YFCMF_VERSION' => 'V1.2.0',
   'THINK_SDK_QQ' => 
   array (
     'APP_KEY' => '203564',
     'APP_SECRET' => 'bab1f3acc0c67b69a1f470ac5e8dc36a',
-    'CALLBACK' => 'http://www.rainfer.cn/ace/index.php?m=Home&c=oauth&a=callback&type=qq',
+    'CALLBACK' => 'http://www.rainfer.cn/index.php?m=Home&c=oauth&a=callback&type=qq',
   ),
   'THINK_SDK_SINA' => 
   array (
     'APP_KEY' => '602735229',
     'APP_SECRET' => '66781cbeab9fdb9b014a387ab6e943fe',
-    'CALLBACK' => 'http://www.rainfer.cn/ace/index.php?m=Home&c=oauth&a=callback&type=sina',
+    'CALLBACK' => 'http://www.rainfer.cn/index.php?m=Home&c=oauth&a=callback&type=sina',
   ),
   'COMMENT' => 
   array (
