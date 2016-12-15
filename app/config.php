4715b6a5677563d02e00bb6ed4a7b3323a8d0537
Rainfer <81818832@qq.com>
2016-12-15 09:43:57 +0800

    增加404页面自动返回

--- app/config.php
+++ app/config.php
@@ -157,7 +157,7 @@ $configs= [
   // +----------------------------------------------------------------------
 
   // 异常页面的模板文件
-    'exception_tmpl'         => THINK_PATH . 'tpl' . DS . 'think_exception.tpl',
+    'exception_tmpl'         => ROOT_PATH . 'public/yfcmf/error.html',
 
   // 错误显示信息,非调试模式有效
     'error_message'          => '页面错误！请稍后再试～',
